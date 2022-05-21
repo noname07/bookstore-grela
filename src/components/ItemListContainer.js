2b68b1476657a1c414295cc116d37a3fd33ccae1
Lucas Grela <grela.lucas@gmail.com>
2022-05-21 15:45:47 -0300

    ItemListContainer added

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,11 @@
+import './ItemListContainer.css';
+
+function ItemListContainer() {
+    return (
+        <h1 className='container'>
+            Hello World!
+        </h1>
+    );
+}
+
+export default ItemListContainer;
