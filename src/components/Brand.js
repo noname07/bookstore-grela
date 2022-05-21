ae21e2487901f8038e3ccbaa7d066ea0803ef449
Lucas Grela <grela.lucas@gmail.com>
2022-05-21 15:59:20 -0300

    Brand component added to avoid clutter

---
+++ src/components/Brand.js
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+function Brand() {
+    return (
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Bookstore
+        </Typography>
+    )
+}
+
+export default Brand;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,13 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar } from "@mui/material";
 import { Box } from "@mui/system";
+import Brand from "./Brand";
 
 function NavBar() {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Bookstore
-                    </Typography>
+                    <Brand />
                 </Toolbar>
             </AppBar>
         </Box>
