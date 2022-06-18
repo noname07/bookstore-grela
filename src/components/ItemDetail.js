c3300c11f5a8054b1da163288c93d1758e0584de
Lucas Grela <grela.lucas@gmail.com>
2022-06-18 15:59:05 -0300

    Added a simple buy button

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,11 +1,15 @@
 import './styles.css';
-import { CardContent, CardMedia, Typography } from '@mui/material';
+import { Button, CardContent, CardMedia, Typography } from '@mui/material';
 import { CartContext } from '../App';
 import { Box } from '@mui/system';
 import { useContext } from 'react';
 
 function ItemDetail({ id, title, description, author, price, img, isDesktop }) {
     const cartContext = useContext(CartContext);
+
+    const buy = function (id) {
+        console.log(`To cart ${id}`);
+    }
 
     return (
         <Box key={id} sx={{ display: 'flex', flexDirection: isDesktop ? 'row' : 'column', border: '.1rem solid #e2e2e2' }}>
@@ -25,7 +29,7 @@ function ItemDetail({ id, title, description, author, price, img, isDesktop }) {
                 <Typography sx={{ textAlign: 'justify' }}>
                     {description}
                 </Typography>
-
+                <Button onClick={() => buy(id)} variant="contained">Buy</Button>
             </CardContent >
             <Typography pr='1rem' alignSelf='end' variant='h6'>
                 US${price}
